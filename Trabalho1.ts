ae33dbb0f1b15405229150c03aca1701b8511b88
acksthe <sthephanifernandes05@gmail.com>
2025-11-16 17:46:31 -0400

    atualização código

--- Trabalho1.ts
+++ Trabalho1.ts
@@ -9,14 +9,18 @@ type Recheio = 'Carne Seca com Cream Cheese'|'Frango Empanado'|'Frango Defumado 
 type Queijo = 'Cheddar'|'Suíço'|'Mussarela (ralada)';
 type Salada = 'Alface'|'Tomate'|'Azeitona Preta'|'Picles'|'Pepinos'|'Pimentão'|'Cebola Roxa'|'Rúcula';
 type Molho = 'Cebola Aguidoce'|'Chipotle'|'Barbecue'|'Parmesão'|'Maionese Temperada'|'Mostarda e Mel'|'Supreme';
+type Adicional = 'Bacon'|'Pepperoni'|'Presunto'|'Salame'|'Cheddar Cremoso';
 
 class Lanche {
     constructor(
         public pao: Pao,
         public recheio: Recheio,
-        public queijo: Queijo,
-        public salada: Salada[],
-        public molho: Molho
+        public queijo?: Queijo,
+        public salada1?: Salada,
+        public salada2?: Salada,
+        public salada3?: Salada,
+        public molho?: Molho,
+        public adicional?: Adicional
     ) {}
 }
 
@@ -27,9 +31,6 @@ class LancheBuilder {
         this.lanche = new Lanche(
             'Parmesão e Orégano',
             'Steak Churrasco',
-            'Cheddar',
-            ['Alface','Tomate','Cebola Roxa'],
-            'Barbecue'
         );
     }
 
@@ -48,13 +49,28 @@ class LancheBuilder {
         return this;
     }
 
-    addSalada(salada: Salada) {
-        this.lanche.salada.push(salada);
+    setSalada1(salada1: Salada) {
+        this.lanche.salada1 = salada1;
+        return this;
+    }
+
+    setSalada2(salada2: Salada) {
+        this.lanche.salada2 = salada2;
+        return this;
+    }
+
+    setSalada3(salada3: Salada) {
+        this.lanche.salada3 = salada3;
         return this;
     }
 
     setMolho(molho: Molho) {
         this.lanche.molho = molho;
+        return this;
+    }
+
+    setAdicional(adicional: Adicional) {
+        this.lanche.adicional = adicional;
         return this;
     }
 
@@ -67,10 +83,11 @@ const lanche1 = new LancheBuilder()
     .setPao('Integral')
     .setRecheio('Frango Defumado com Cream Cheese')
     .setQueijo('Suíço')
-    .addSalada('Rúcula')
-    .addSalada('Pepinos')
-    .addSalada('Tomate')
+    .setSalada1('Rúcula')
+    .setSalada2('Pepinos')
+    .setSalada3('Tomate')
     .setMolho('Mostarda e Mel')
+    .setAdicional('Bacon')
     .build();
 console.log(lanche1);
 
@@ -78,9 +95,9 @@ const lanche2 = new LancheBuilder()
     .setPao('Três Queijos')
     .setRecheio('Frango Empanado')
     .setQueijo('Mussarela (ralada)')
-    .addSalada('Alface')
-    .addSalada('Cebola Roxa')
-    .addSalada('Azeitona Preta')
+    .setSalada1('Alface')
+    .setSalada2('Cebola Roxa')
+    .setSalada3('Azeitona Preta')
     .setMolho('Chipotle')
     .build();
 console.log(lanche2);
@@ -89,9 +106,10 @@ const lanche3 = new LancheBuilder()
     .setPao('Três Queijos')
     .setRecheio('Vegetariano')
     .setQueijo('Suíço')
-    .addSalada('Alface')
-    .addSalada('Tomate')
-    .addSalada('Pepinos')
+    .setSalada1('Alface')
+    .setSalada2('Tomate')
+    .setSalada3('Pepinos')
     .setMolho('Cebola Aguidoce')
+    .setAdicional('Cheddar Cremoso')
     .build();
 console.log(lanche3);
